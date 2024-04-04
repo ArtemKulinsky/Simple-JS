d70ddddcfdc3453a5b6817f636dfe14d3d90aaf2
artemkulinskii <a.kulinscky1@gmail.com>
2024-04-04 15:20:28 +0300

    Configured dev environment

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  singleQuote: true,
+  proseWrap: 'always',
+  singleAttributePerLine: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  endOfLine: 'crlf',
+};
---
+++ Readme.md
@@ -0,0 +1,6 @@
+## Project init
+1. npm i
+
+
+## Project start
+1. npm run start
